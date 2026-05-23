12d1bc77aff5aa6b8d16d43659e53e6292d71b6c
Gianluca Bianchini <gianluca.bianchini@idna.it>
2026-05-23 15:31:46 +0200

    feature(footer): added first infos in footer

--- web/app/themes/pauperopoleague/resources/views/sections/footer.blade.php
+++ web/app/themes/pauperopoleague/resources/views/sections/footer.blade.php
@@ -1,3 +1,7 @@
 <footer class="content-info">
   @php(dynamic_sidebar('sidebar-footer'))
+  <div class="site-footer-credits">
+    <p>Pauperopoleague @ Empoli (Italy) since 2026</p>
+    <p>Made with ❤️ by Gianluca Bianchini</p>
+  </div>
 </footer>
